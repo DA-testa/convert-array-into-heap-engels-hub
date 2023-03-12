4b719c7c1316f9bd61fa6a5fd17c928b4d8956fc
Artjoms Papšs <68332986+engels-hub@users.noreply.github.com>
2023-03-12 09:21:08 +0000

    221RDB031

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,61 @@
 # python3
-
+"""placeholder docstring so lint stops screaming"""
+import math
 
 def build_heap(data):
+    """placeholder docstring so lint stops screaming"""
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
+    for i in range(math.floor(len(data)/2), -1, -1):
+        sift(swaps,data,i)
 
     return swaps
 
+def sift(swaps,arr, i):
+    """placeholder docstring so lint stops screaming"""
+    _l = 2*i+1
+    j = i
+    if _l < len(arr) and arr[_l] < arr[j]:
+        j=_l
+    _r = 2*i+2
+    if _r < len(arr) and arr[_r] < arr[j]:
+        j=_r
+    if i != j:
+        swaps.append((i, j))
+        arr[i], arr[j] = arr[j], arr[i]
+        sift(swaps, arr, j)
+
+
+
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
+    """placeholder docstring so lint stops screaming"""
+    # add another input for I or F
     # first two tests are from keyboard, third test is from a file
+    inp=input()
+
+    if 'I' in inp:
+        numbers = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == numbers
+    else:
+        file_n = input()
+        if 'a' in file_n:
+            print('invalid file')
+            return
+        file=open(f"tests/{file_n}","r", encoding="utf-8")
+        numbers = int(file.readline().strip())
+        data =list(map(int,file.readline().strip('\n').split(" ")))
 
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
+    # calls function to assess the data
     # and give back all swaps
     swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
 
 
     # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
 
 if __name__ == "__main__":
     main()
